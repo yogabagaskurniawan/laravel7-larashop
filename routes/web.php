1830f65b12018d69be7a739586e011a888b2d1b8
yogabagaskurniawan <yogabag4s@gmail.com>
2023-06-19 22:11:20 +0700

    memodifikasi routes dan memasang templete admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(
+    ['namespace' => 'admin', 'prefix' => 'admin'],
+    function () {
+        Route::get('dashboard', 'DashboardController@index');
+    }
+);
