5b156ea2b6fd5dfdc6c7f4f11f6c456c82ce45c8
yogabagaskurniawan <yogabag4s@gmail.com>
2023-06-22 11:14:38 +0700

    Manajemen Produk - Pembuatan Table Database

---
+++ database/migrations/2023_06_22_040421_create_product_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id');   
+            $table->text('path');
+            $table->timestamps();
+
+            // isi tabel ini akan otomatis terhapus jika tabel products dihapus
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+    }
+}
